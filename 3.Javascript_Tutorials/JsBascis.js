3090a9f523df7ba23c4636554f421349dd48b2cc
shiransh <sp999pratap@gmail.com>
2023-04-07 11:03:34 +0530

    updated

---
+++ 3.Javascript_Tutorials/JsBascis.js
@@ -0,0 +1,30 @@
+//js is a client side scripting language(form gets submitted when you click on a button)
+//It is used to make the web pages alive
+//It used to perform the action using programming in web pages
+//intially it was called as the livescript
+//DOM - document object model (page),it means the page is avilable to use in dom form, it all includes the html ,css,body,tag etc.
+//Also used in Ajax request (used to help in without loading the page made changes in it dynamically)
+//Js is also used to take the data to the server.
+//Js can used not only on browser but also on the server
+//Js can be used a client as well as server side language.
+
+//Brower -> Javascript -> executes the code
+//What In-Browser can do?
+
+// 1.Add new HTML and change existing html from DOM.
+// 2.React to Events.(any response from server),like you pressed a key or moved a mouse on page.
+// 3.Ajax requests
+// 4.Get and set cookies realted to your websites and use local storage in javascript.
+
+//What  In-browser Js cannot do?
+
+//1.Read/write from computer hardisk cannot be performed.
+//2.Cross side data transfer is not allowed between two websites.(Known as the same origin policy,we'll get the same origin policy error in such case).
+//3.With the help of some headers this type of handshake is possible.
+//4.Javascript is a unique programming language (it contains the support of HTML/Css)
+//5.It provides simple functions/simple APIs.
+//6.Javascript is maintained by it's community,where ECMA provides the latest updates/versions.
+
+//Versions: latest versions is : ECMA 2015,16,17,18,19 
+// Ecma script is known as ES => ES 6 updates like this.
+
